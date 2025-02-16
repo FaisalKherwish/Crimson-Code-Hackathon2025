57f5bc5ee37b36fce6856d67d1a73fedd479e89a
Allen Yang <155710586+defilement@users.noreply.github.com>
2025-02-16 05:39:41 -0800

    Update main.c

--- Inspiration Quest/Inspiration Quest/main.c
+++ Inspiration Quest/Inspiration Quest/main.c
@@ -4,10 +4,14 @@ int main(void)
 {
 	srand(time(NULL));
 	int status = -1;
+	double quota = 100;
+
+	printMenu();
 
 	do
 	{
-		status = game();
+		status = game((int)quota);
+		quota *= 1.2;
 	} while (status != 0);
 	
 	printf("Thanks for playing, go discover your own inspirations!");
