92e664cbe8488e4d1033994d30236db9900a391c
iDataWhnUsllp <152748701+iDataWhnUsllp@users.noreply.github.com>
2025-02-15 21:22:42 -0800

    Update Header
    Basic struct for inspiration backpack and artpiece

--- Inspiration Quest/Inspiration Quest/Header.h
+++ Inspiration Quest/Inspiration Quest/Header.h
@@ -1,1 +1,37 @@
+#ifndef INSPIRATION_H
+#define INSPIRATION_H
+
+typedef struct inspiration {
+	int community;
+	int dailyLife;
+	int nature;
+	int tech;
+	int entertainment;
+	int quality;
+} Inspiration;
+typedef struct artPiece {
+	int value;
+} ArtPiece;
+computeSale(double multiplier);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#endif
 
