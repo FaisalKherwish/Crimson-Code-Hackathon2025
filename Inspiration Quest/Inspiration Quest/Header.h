b9fbf18cfb163ba0fa99532b2d558b4c2f803415
iDataWhnUsllp <152748701+iDataWhnUsllp@users.noreply.github.com>
2025-02-16 00:51:03 -0800

    Update Header.h

--- Inspiration Quest/Inspiration Quest/Header.h
+++ Inspiration Quest/Inspiration Quest/Header.h
@@ -44,6 +44,7 @@ void enviormentPicker();
 
 
 
+#endif
 
 
 
@@ -55,11 +56,80 @@ void enviormentPicker();
 
 
 
+#ifndef INSPIRATION_H
+#define INSPIRATION_H
+#include <stdio.h>
+#include <ctype.h>
+#include <string.h>
 
 
+//computeSale(double multiplier);
+typedef struct artPiece {
+	int value;
+} ArtPiece;
+typedef struct node
+{
+	ArtPiece data;
+	struct node* pNext;
+} Node;
+
+typedef struct area {
+	char envrionment; //forest
+	//char O;  //ourskirts
+	//char D;  //downtown
+	//char C; //club
+} Area;
 
 
+typedef struct inspiration {
+	int community;
+	int dailyLife;
+	int nature;
+	int tech;
+	int entertainment;
+	int ethos;
+	int quality;
+} Inspiration;
+
+//typedef struct inspiration
+//{
+//	int community;
+//	int dailyLife;
+//	int nature;
+//	int tech;
+//	int entertainment;
+//	int quality;
+//} Inspiration;
+
+//typedef struct artPiece
+//{
+//	int value;
+//} ArtPiece;
+
+typedef struct inspiration
+{
+	int quality;
+	char* grouping;
+}Inspiration;
+
+typedef struct node
+{
+	Inspiration data;
+	struct node* pNext;
+	struct node* pPrev;
+}Node;
+
+Node* createNode(Inspiration newData);
+void insertInList(Node** pList, Inspiration newData);
+void transferData(Node** pList, Node* pMem);
+void deleteList(Node** pList);
+void printNode(Node* pCur);
+double lookForGrouping(Node* pList);
+double groupingValue(char* grouping, int maxCount, int size);
+double qualityCalculate(Node* pList);
+computeSale(double multiplier);
+void printMenu();
+void enviormentPicker();
 
 
 #endif
-
