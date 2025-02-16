bb96867c1201be50f498e6f2505c1a4cb51e8443
iDataWhnUsllp <152748701+iDataWhnUsllp@users.noreply.github.com>
2025-02-16 04:06:01 -0800

    Update main.c

--- Inspiration Quest/Inspiration Quest/main.c
+++ Inspiration Quest/Inspiration Quest/main.c
@@ -11,3 +11,26 @@ int main(void)
 	
 	printf("Thanks for playing, go discover your own inspirations!");
 }
+
+
+
+
+
+
+
+
+
+
+#include "fuk.h"
+
+int main(void)
+{
+	int status = -1;
+
+	do
+	{
+		status = game();
+	} while (status != 0);
+
+	printf("Thanks for playing, go discover your own inspirations!");
+}
